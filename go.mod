bdb8e6479e7deb91b38dcd208579c436555444c9
WitheringAway <124115470+notwithering@users.noreply.github.com>
2024-05-27 23:27:13 -0400

    made a completely working text box

--- go.mod
+++ go.mod
@@ -5,18 +5,12 @@ go 1.22.3
 require github.com/gobuffalo/envy v1.10.2
 
 require (
-	github.com/gdamore/encoding v1.0.0 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/uniseg v0.4.3 // indirect
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
 	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
 )
 
 require (
-	github.com/gdamore/tcell/v2 v2.7.4
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/notwithering/sgr v0.0.0-20240525174708-d8436a98dce9
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
-	golang.org/x/term v0.20.0
 )
--- input.go
+++ input.go
@@ -1,46 +1,118 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"strconv"
 
-	"github.com/gdamore/tcell/v2"
-	"golang.org/x/term"
+	"github.com/eiannone/keyboard"
 )
 
-// FIXME: make this more like a text box (add backspace, cursor control, keybinds, etc.)
 func line(password bool) (string, error) {
-	state, err := term.MakeRaw(int(os.Stdin.Fd()))
-	if err != nil {
+	if err := keyboard.Open(); err != nil {
 		return "", err
 	}
-	defer term.Restore(int(os.Stdin.Fd()), state)
+	defer keyboard.Close()
 
 	var input []byte
+	var position int
 
-	reader := bufio.NewReader(os.Stdin)
+	var reprintFromPosition = func() {
+		var distance int = len(input[position:]) + 1
+		if password {
+			for range distance - 1 {
+				fmt.Print("*")
+			}
+		} else {
+			fmt.Print(string(input[position:]))
+		}
+		fmt.Print(" ")
+		for range distance {
+			fmt.Print("\x1b[D")
+		}
+	}
+
 	for {
-		r, _, err := reader.ReadRune()
+		r, key, err := keyboard.GetKey()
 		if err != nil {
 			return "", err
 		}
 
-		if r == rune(tcell.KeyEnter) {
+		switch key {
+		case keyboard.KeyEnter:
+			fmt.Print("\r\n")
+			fmt.Print(string(input))
 			fmt.Print("\r\n")
 			return string(input), nil
-		}
+		case keyboard.KeyArrowLeft:
+			if position > 0 {
+				position--
+				fmt.Print("\x1b[D")
+			}
+		case keyboard.KeyArrowRight:
+			if position < len(input) {
+				position++
+				fmt.Print("\x1b[C")
+			}
+		case keyboard.KeyBackspace, keyboard.KeyBackspace2:
+			if len(input) > 0 && position > 0 {
+				var inputBuf []byte
+				for i, r := range input {
+					if i != position-1 {
+						inputBuf = append(inputBuf, r)
+					}
+				}
+				input = inputBuf
+				position--
+				fmt.Print("\x1b[D")
 
-		if r < ' ' || r > '~' {
-			continue
-		}
+				reprintFromPosition()
+			}
+		case keyboard.KeyDelete:
+			if len(input) > 0 && position < len(input) {
+				var inputBuf []byte
+				for i, r := range input {
+					if i != position {
+						inputBuf = append(inputBuf, r)
+					}
+				}
+				input = inputBuf
 
-		input = append(input, byte(r))
-		if password {
-			fmt.Print("*")
-		} else {
-			fmt.Print(string(r))
+				reprintFromPosition()
+			}
+		case keyboard.KeyCtrlA:
+			for range position {
+				fmt.Print("\x1b[D")
+			}
+			position = 0
+		case keyboard.KeyCtrlE:
+			for range len(input) - position {
+				fmt.Print("\x1b[C")
+			}
+			position = len(input)
+		// TODO: Add CTRL+U, CTRL+K, and CTRL+W
+		default:
+			if key == keyboard.KeySpace {
+				r = ' '
+			}
+
+			if r < ' ' || r > '~' {
+				continue
+			}
+
+			inputBuf := append(input, 0)
+			copy(inputBuf[position+1:], inputBuf[position:])
+			inputBuf[position] = byte(r)
+			input = inputBuf
+
+			if password {
+				fmt.Print("*")
+			} else {
+				fmt.Print(string(r))
+			}
+
+			position++
+
+			reprintFromPosition()
 		}
 	}
 }
