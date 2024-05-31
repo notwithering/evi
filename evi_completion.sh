55f4522990010740291e6caa74829bcd33f230eb
WitheringAway <124115470+notwithering@users.noreply.github.com>
2024-05-31 05:09:56 -0400

    add optional bash completion

---
+++ evi_completion.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+_evi_completions() {
+	local cur prev opts
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
+	opts="-k -key -no-decrypt -no-edit -no-encrypt -help"
+	doubleopts="--no-decrypt --no-edit --no-encrypt --help"
+
+	if [[ ${cur} == --* ]]; then
+		COMPREPLY=( $(compgen -W "${doubleopts}" -- ${cur}) )
+		return 0
+	fi
+	if [[ ${cur} == -* ]]; then
+		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+		return 0
+	fi
+
+	COMPREPLY=( $(compgen -f -- ${cur}) )
+	return 0
+}
+
+complete -F _evi_completions evi
